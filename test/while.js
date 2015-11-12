c141d90b11798e4c62fc67a2c57ec81c26e3dde7
Stefan Oravec <stefan.oravec@me.com>
2015-11-12 11:31:07 +0100

    should propagate rejection - test

--- test/while.js
+++ test/while.js
@@ -1,21 +1,46 @@
-var Promise      = require('bluebird')
-var promiseWhile = require(__dirname+'/../')(Promise) 
+var Promise = require('bluebird');
+var promiseWhile = require(__dirname + '/../')(Promise);
+var should = require('should');
 
-describe('Promise While', function() {
+describe('Promise While', function () {
 
-  it('should loop three times asynchronously', function(done) {
+  it('should loop three times asynchronously', function (done) {
     var i = 0;
 
-    promiseWhile(function() { return i < 3 },
-      function() {
-        return new Promise(function(resolve, reject) {
-          setTimeout(function() {
-            console.log('aye', i)
-            resolve(i++)
+    promiseWhile(function () {
+        return i < 3
+      },
+      function () {
+        return new Promise(function (resolve, reject) {
+          setTimeout(function () {
+            console.log('aye', i);
+            resolve(i++);
           }, 500)
         })
-    })
-    .then(function() { done() })
+      })
+      .then(function () {
+        done();
+      })
   });
-})
+
+  it('should propagate rejection', function (done) {
+
+    function TestError() {}
+
+    promiseWhile(function () {
+        return true;
+      },
+      function () {
+        return new Promise(function (resolve, reject) {
+          reject(new TestError());
+        });
+      })
+      .catch(function (err) {
+        err.should.be.instanceof(TestError);
+        done();
+      })
+    ;
+  });
+
+});
 
